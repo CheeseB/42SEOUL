a5339ca906d292ca90d751e7a575c3873a403225
Seongyeon Kim <seongkim@c4r5s1.42seoul.kr>
2020-04-13 23:46:51 +0900

    modifieg

--- 42cursus/Libft/ft_itoa.c
+++ 42cursus/Libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: seongkim <seongki@student.42seoul.kr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/11 19:23:51 by seongkim          #+#    #+#             */
-/*   Updated: 2020/04/11 19:23:53 by seongkim         ###   ########.fr       */
+/*   Updated: 2020/04/13 23:43:34 by seongkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,46 +19,28 @@ static int	get_length(int n)
 	return (1);
 }
 
-static int	get_digit(int len)
-{
-	if (len - 1)
-		return (10 * get_digit(len - 1));
-	return (1);
-}
-
-static void	fill_ptr(char *ptr, int digit, int n, int i)
-{
-	while (digit)
-	{
-		ptr[i++] = (n / digit) + '0';
-		n %= digit;
-		digit /= 10;
-	}
-	ptr[i] = 0;
-}
-
 char		*ft_itoa(int n)
 {
 	char	*ptr;
 	int		len;
 	int		digit;
-	int		i;
 
 	if (n == -2147483648)
 		return ("-2147483648");
 	len = get_length(n);
 	if (n < 0)
+	{
 		len++;
+		n *= -1;
+	}
 	if (!(ptr = (char *)malloc(sizeof(char) * (len + 1))))
 		return (0);
-	i = 0;
 	if (n < 0)
+		ptr[0] = '-';
+	while (n)
 	{
-		len--;
-		n *= -1;
-		ptr[i++] = '-';
+		ptr[--len] = (n % 10) + '0';
+		n /= 10;
 	}
-	digit = get_digit(len);
-	fill_ptr(ptr, digit, n, i);
 	return (ptr);
 }
